1e3fdf564d5646dc1290e928d246682edbca3f08
MarijnVerschuren <marijnverschuren3@gmail.com>
2024-04-16 21:18:33 +0200

    Updated update.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,16 @@
+cd STM32H7B0_CMSIS
+git submodule update --init
+cd ..
+
 rm ./soft/doc -rf
 rm ./soft/include -rf
 rm ./soft/src/*.c
 rm ./soft/src/usb -rf
+rm ./soft/test -rf
 
 cp ./STM32H7B0_CMSIS/doc ./soft/doc -r
 cp ./STM32H7B0_CMSIS/include ./soft/include -r
 cp ./STM32H7B0_CMSIS/src/* ./soft/src/ -r
 rm ./soft/src/main.c
+cp ./STM32H7B0_CMSIS/test ./soft/test -r
+
