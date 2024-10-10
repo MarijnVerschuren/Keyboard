4554d95ff49b8df006b803ad567cec7caa052773
MarijnVerschuren <marijnverschuren3@gmail.com>
2024-10-10 23:38:01 +0200

    Bugfix

--- update.sh
+++ update.sh
@@ -9,8 +9,6 @@ rm ./software/src -rf
 rm ./software/targets -rf
 rm ./software/cmake -rf
 rm ./software/.run -rf
-rm ./software/CMakeLists.txt
-rm ./software/.gitignore
 
 cp ./STM32F412/inc ./software/ -r
 cp ./STM32F412/lib ./software/ -r
@@ -19,8 +17,6 @@ cp ./STM32F412/src ./software/ -r
 cp ./STM32F412/targets ./software/ -r
 cp ./STM32F412/cmake ./software/ -r
 cp ./STM32F412/.run ./software/ -r
-cp ./STM32F412/CMakeLists.txt ./software/
-cp ./STM32F412/.gitignore ./software/
 
 rm ./software/src/main.c
 mv main.c ./software/src
