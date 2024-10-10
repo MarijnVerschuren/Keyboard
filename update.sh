142071aa98f7c28d631561532d39a063e30c9e0c
MarijnVerschuren <marijnverschuren3@gmail.com>
2024-10-10 23:36:41 +0200

    Added software

--- update.sh
+++ update.sh
@@ -7,14 +7,20 @@ rm ./software/lib -rf
 rm ./software/lnk -rf
 rm ./software/src -rf
 rm ./software/targets -rf
+rm ./software/cmake -rf
 rm ./software/.run -rf
+rm ./software/CMakeLists.txt
+rm ./software/.gitignore
 
 cp ./STM32F412/inc ./software/ -r
 cp ./STM32F412/lib ./software/ -r
 cp ./STM32F412/lnk ./software/ -r
 cp ./STM32F412/src ./software/ -r
 cp ./STM32F412/targets ./software/ -r
-cp ./STM32F412/.run ./software/ -rf
+cp ./STM32F412/cmake ./software/ -r
+cp ./STM32F412/.run ./software/ -r
+cp ./STM32F412/CMakeLists.txt ./software/
+cp ./STM32F412/.gitignore ./software/
 
 rm ./software/src/main.c
 mv main.c ./software/src
